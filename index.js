a1f3646bd49a8808928d57944c44d83d62964eb4
Oscar Tovar <oscartovar@localhost.localdomain>
2019-07-09 22:04:39 -0500

    Update DawgBot to send memes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
+const request = require('request');
 
 const client = new Discord.Client();
+const url = 'https://www.reddit.com/r/dankmemes/top.json?limit=3&t=hour';
 const { DISCORD_TOKEN } = process.env;
+const { prefix } = require('./config');
 
 client.once('ready', () => {
   if (DISCORD_TOKEN) {
@@ -11,8 +14,20 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
-  if (message.content === '!!!Dawg') {
+  if (message.content === `${prefix}`) {
     message.channel.send('Yo Dawg. It\'s me DawgBot');
+  }
+  if (message.content === `${prefix} memes`) {
+    request.get(url, (_err, _response, body) => {
+      const memes = JSON.parse(body).data.children;
+      for (let i = 0; i < memes.length; i += 1) {
+        const response = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(`Fresh Dank Meme #${i + 1}`)
+          .setImage(memes[i].data.url);
+        message.channel.send(response);
+      }
+    });
   }
 });
 
